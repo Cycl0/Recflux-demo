9fa783cf5cff0dd5f9f66b883f712986032f6ff0
Cycl0 <lucas.cyra@etec.sp.gov.br>
2024-08-23 23:14:03 -0300

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import InputBox from "@/components/InputBox";
 import GeneratedSection from "@/components/GeneratedSection";
 import VideoBackground from '@/components/VideoBackground';
+import NavBar from '@/components/NavBar';
 
 export default function Home() {
   const [index, setIndex] = useState(-1);
@@ -12,6 +13,8 @@ export default function Home() {
     setIndex((prevIndex) => prevIndex + 1);
   }
     return (
+      <>
+        <NavBar />
         <main className="xl:!p-36 lg:!p-12 md:!p-8 sm:!p-4">
             <VideoBackground />
             <div id="content" className={`min-h-screen items-center justify-between py-20 px-12 backdrop-blur-2xl opacity-[99%] shadow-gradient-2 rounded-md`}>
@@ -19,5 +22,6 @@ export default function Home() {
                 {(index > -1) && <GeneratedSection index={index} />}
             </div>
         </main>
+        </>
     );
 }
