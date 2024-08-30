89e112c38a0c5491806c2d4a06f2de50fe87385a
Cycl0 <lucas.cyra@etec.sp.gov.br>
2024-08-30 16:30:18 -0300

    add to background rotate 180 & color animation

--- app/components/VideoBackground.tsx
+++ app/components/VideoBackground.tsx
@@ -1,18 +1,18 @@
 const VideoBackground = () => {
 
         return (
-          <div className="video-background brightness-200">
+        <div className="video-background brightness-200 rotate-180">
             <video
-              muted
-              playsInline
-              loop
-              autoPlay
+                muted
+                playsInline
+                loop
+                autoPlay
             >
-              <source src="/particles-blue.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
+                <source src="/particles-blue.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
             </video>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default VideoBackground;
